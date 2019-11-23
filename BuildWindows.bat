a250a74f560b9515ab272675e2697cf98ddab350
RPG3D <imxqliu@live.com>
2019-11-24 00:00:37 +0800

    1.build as C API

--- BuildWindows.bat
+++ BuildWindows.bat
@@ -1,5 +1,9 @@
 
-xmake  f -p windows
+xmake  f -p windows -m debug
+xmake -b
+
+
+xmake  f -p windows -m release
 xmake -b
 
 pause
--- xmake.lua
+++ xmake.lua
@@ -2,16 +2,30 @@
 
 
 target("LZ4")
-	set_kind("static")
+	set_kind("shared")
 	set_languages("c11", "cxx14")
+
     add_files("lib/*.c")
 
+	add_rules("mode.debug", "mode.release")
+
+	if is_mode("debug") then
+		set_symbols("debug")
+		set_optimize("none")
+	else
+		set_optimize("fastest")
+	end
+
     if (is_os("windows")) then
-		set_kind("shared")
+		add_cxflags("/TC", {force = true})
 		add_defines("LZ4_DLL_EXPORT=1")
 	end
 	
 	
 	if (is_os("android")) then
-		set_kind("shared")
+	
+	end
+
+	if (is_os("ios")) then
+		set_kind("static")
 	end
