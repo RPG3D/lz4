572d0d5e9354bdf60fe97bd2faf34da25c55cf43
RPG3D <imxqliu@live.com>
2019-12-25 22:04:47 +0800

    1.add Android x86_64

--- BuildAndroid.bat
+++ BuildAndroid.bat
@@ -1,10 +1,12 @@
 
-xmake  f -p android  --android_sdk="%ANDROID_HOME%"  --ndk="%ANDROID_NDK_HOME%"  -a armv7-a --ndk_cxxstl=gnustl_shared
+xmake  f -p android  --android_sdk="%ANDROID_HOME%"  --ndk="%ANDROID_NDK_ROOT%"  -a armv7-a --ndk_cxxstl=gnustl_shared
 xmake -b
 
-xmake  f -p android  --android_sdk="%ANDROID_HOME%"  --ndk="%ANDROID_NDK_HOME%"  -a arm64-v8a --ndk_cxxstl=gnustl_shared
+xmake  f -p android  --android_sdk="%ANDROID_HOME%"  --ndk="%ANDROID_NDK_ROOT%"  -a arm64-v8a --ndk_cxxstl=gnustl_shared
 xmake -b
 
+xmake  f -p android  --android_sdk="%ANDROID_HOME%"  --ndk="%ANDROID_NDK_ROOT%"  -a x86_64 --ndk_cxxstl=gnustl_shared
+xmake -b
 
 pause
 
